87e5f64fc80f3b53d00b8d3a7d60efe76bece075
Yusup366 <morgulak65@gmail.com>
2024-10-31 20:47:00 +0600

    day1

---
+++ day1.py
@@ -0,0 +1,32 @@
+# ооп
+#
+# types_python = 1, 'str', 1.2, True, {}, [], ()
+#
+# print(type(types_python))
+
+
+class Car:
+
+
+
+    def drive(self):
+        print('машина едит',self.model)
+
+    def __init__(self,model,volume,age):
+        self.model = model
+        self.volume = volume
+        self.age = age
+
+    def start(self):
+        print('машина завелась',self.age)
+
+    def __str__(self):
+        return f'{audi.model} {audi.volume}'
+
+#экземпяр
+audi = Car(model='a8',age='2000',volume='2.8')
+mazda = Car(model='rx-8',age='2001',volume='1.5')
+audi.drive()
+mazda.drive()
+
+print(audi)
